b42a9895131752c4cc1cd94bb6909cf6f6bd0a4a
josed18 <joseandresd18@gmail.com>
2020-01-12 11:37:10 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='books.api',
+   version='1.0',
+   description='A useful module',
+   author='Jose Delgado',
+   author_email='joseandresd18@gmail.com',
+   packages=['books-api'],
+   scripts=[
+            'scripts/cool',
+            'scripts/skype',
+           ]
+)
